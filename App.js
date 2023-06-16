2ab26822171d303b29438934bce7c6ee633b55b6
TehillahAddy <90287225+TehillahAddy@users.noreply.github.com>
2023-06-16 20:24:03 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -5,15 +5,14 @@ import LoginForm from './components/LoginForm';
 import LandingPage from './components/LandingPage';
 import SignUpPage from './components/SignUpPage';
 import HomePage from './components/HomePage';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Route path="/" element={<Navigate to="/landingpage" />} />} />
-          <Route path="/landingpage" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/loginform" element={<LoginForm />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/home" element={<HomePage />} />
