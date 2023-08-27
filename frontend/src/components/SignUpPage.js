66aa0e52e728f3d9c186db90925d0dc17173dde9
Sosu Alfred <sosualfred@gmail.com>
2023-08-27 20:36:04 +0000

    integrate signup api

--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -2,66 +2,71 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; // Import Axios for making API requests
 import './SignUp.css';
+import { toast } from 'react-toastify';
 
 const SignupPage = () => {
   const navigate = useNavigate();
 
-  const [userData, setUserData] = useState({
-    username: '',
-    email: '',
-    phone: '',
-    isBusinessAccount: false,
-    firstName: '',
-    lastName: '',
-    password: '',
-    confirmPassword: '',
-    agreeToTerms: false,
-  });
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [isBusinessAccount, setIsBusinessAccount] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-  const handleInputChange = (event) => {
-    const { id, value, type, checked } = event.target;
-    const newValue = type === 'checkbox' ? checked : value;
-    setUserData((prevData) => ({ ...prevData, [id]: newValue }));
-  };
+  const [isAgreeToTerms, setIsAgreeToTerms] = useState(false);
 
-  const handleSignUp = async () => {
-    if (!userData.agreeToTerms) {
-      // Notify the user about agreeing to terms
-      return;
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!isAgreeToTerms) {
+      return toast.error('Please agree to the terms and conditions');
+    }
+
+    if (password !== confirmPassword) {
+      return toast.error('Passwords do not match');
+    } else if (password.length < 8) {
+      return toast.error('Password must be at least 8 characters');
     }
 
     try {
-      const response = await axios.post('/http://localhost:3000/api/signup', userData);
-      if (response.status === 201) {
-        // User successfully signed up, navigate to login page
+      const response = await axios.post('http://localhost:3001/auth/register', {
+
+        username: userName,
+        email: email,
+        phoneNumber: phoneNumber,
+        isBusiness: isBusinessAccount,
+        firstName: firstName,
+        lastName: lastName,
+        password: password
+      });
+
+      if (response.status === 200) {
+        toast.success('Successfully signed up');
         navigate('/login');
       } else {
-        // Handle other response status codes if needed
+        toast.error('Error signing up');
       }
     } catch (error) {
-      // Handle signup error, display error message to the user
-      console.error('Error signing up:', error);
+      toast.error('Error signing up');
     }
   };
 
-  const handleLogin = () => {
-    // Perform login logic here
-
-    // After successful login, navigate to the homepage
-    navigate('/loginform');
-  };
-
-
-
-
   return (
-    <div>
+    <form onSubmit={(e) => handleSignUp(e)}
+    >
       <h1 className="logo">SurveyAce</h1>
       <div className="signup-page">
         <h2>Sign Up for SurveyAce.</h2>
 
         <div className="username-container">
-          <input type="text" id="username" placeholder="Username:" />
+          <input type="text" id="username" placeholder="Username:" required
+            onChange={(event) => {
+              setUserName(event.target.value);
+            }}
+          />
         </div>
 
         <p className="terms-and-conditionss">
@@ -70,35 +75,58 @@ const SignupPage = () => {
 
         <div className="sub-container">
           <div className="email-container">
-            <input type="email" id="email" placeholder="Email:" />
+            <input type="email" id="email" placeholder="Email:" required
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }} />
           </div>
 
           <div className="phone-container">
-            <input type="tel" id="phone" placeholder="Phonenumber:" />
+            <input type="tel" id="phone" placeholder="Phonenumber:" required
+              onChange={(event) => {
+                setPhoneNumber(event.target.value);
+              }} />
           </div>
         </div>
 
         <div className="business-account-container">
-          <input type="checkbox" id="business-account" />
+          <input type="checkbox" id="business-account" required
+            onChange={(event) => {
+              setIsBusinessAccount(event.target.checked);
+            }} />
           <label htmlFor="business-account">Business Account?</label>
         </div>
 
         <div className="name-container">
           <div className="first-name-container">
-            <input type="text" id="first-name" placeholder="Firstname:" />
+            <input type="text" id="first-name" placeholder="Firstname:" required
+              onChange={(event) => {
+                setFirstName(event.target.value);
+              }} />
           </div>
 
           <div className="last-name-container">
-            <input type="text" id="last-name" placeholder="Lastname:" />
+            <input type="text" id="last-name" placeholder="Lastname:" required
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+            />
           </div>
         </div>
 
         <div className="password-container">
-          <input type="password" id="password" placeholder="Password:" />
+          <input type="password" id="password" placeholder="Password:" required
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }} />
         </div>
 
         <div className="confirm-password-container">
-          <input type="password" id="confirm-password" placeholder="Confirm Password:" />
+          <input type="password" id="confirm-password" placeholder="Confirm Password:" required
+            onChange={(event) => {
+              setConfirmPassword(event.target.value);
+            }}
+          />
         </div>
 
         <p className="terms-and-conditions">
@@ -106,14 +134,19 @@ const SignupPage = () => {
         </p>
 
         <div className="business-account-container">
-          <input type="checkbox" id="business-account" />
+          <input type="checkbox" id="business-account"
+            onChange={(event) => {
+              setIsAgreeToTerms(event.target.checked);
+            }} />
           <label htmlFor="business-account">I agree to the terms and conditions?</label>
         </div>
 
 
-        <button type="submit" onClick={handleLogin}>Sign Up</button>
+        <button id="submitBtn" type="submit">
+          Sign Up
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
