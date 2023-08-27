48de0683a6e446b4ebfc245a3659eb8e0b58d3d8
Sosu Alfred <sosualfred@gmail.com>
2023-08-27 21:05:48 +0000

    integrate login api

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const LoginForm = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Perform login logic here
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-    // After successful login, navigate to the homepage
-    navigate('/home');
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', {
+        username: username,
+        password: password
+      });
+
+      if (response.status === 200) {
+        toast.success('Successfully logged in');
+        navigate('/home');
+      } else {
+        toast.error('Invalid username or password');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Invalid username or password');
+    }
+
   };
 
   return (
-    <div>
+    <form onSubmit={(e) => handleLogin(e)}>
       <h1 className="logo">SurveyAce</h1>
       <div className="login-page">
         <h2>Login</h2>
 
         <div className="username-container">
-          <input type="text" id="username" placeholder="Username:" />
+          <input type="text" placeholder="Username:" onChange={(e) => setUsername(e.target.value)} />
         </div>
 
         <div className="password-container">
-          <input type="password" id="password" placeholder="Password:" />
+          <input type="password" placeholder="Password:" onChange={(e) => setPassword(e.target.value)} />
         </div>
 
-        <div className="remember-me-container">
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Remember Me</label>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <button type="submit">Login</button>
         </div>
-
-        <button type="submit" onClick={handleLogin}>Login</button>
-
-        <p className="terms-and-conditions">
-          Forgot Password?
-        </p>
 
         <p className="terms-and-conditions">
           <a href="/signup">Sign Up for free</a>
@@ -44,7 +57,7 @@ const LoginForm = () => {
       <div className="footer">
         Version 1.1
       </div>
-    </div>
+    </form>
   );
 };
 
